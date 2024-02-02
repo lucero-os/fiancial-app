edde669c814e73bab4478379a3d138d17e341c87
Osvaldo lucero <osvaldolcuero98@gmail.com>
2024-02-02 15:59:33 -0300

    CREATE UserDTO, CreaateUserWithNotifictionDTO dtos

---
+++ src/main/java/com/example/financial_app/DTOs/User/CreateUserWithNotificationDTO.java
@@ -0,0 +1,58 @@
+package com.example.financial_app.DTOs.User;
+
+import java.util.List;
+
+public class CreateUserWithNotificationDTO {
+    private String name;
+    private String surname;
+    private String mail;
+    private String password;
+    List<Long> notificationIds;
+
+    public CreateUserWithNotificationDTO(){}
+    
+    public CreateUserWithNotificationDTO(String name, String surname, String mail, String password, List<Long> notificationIds) {
+        this.setName(name);
+        this.setSurname(surname);
+        this.setMail(mail);
+        this.setPassword(password);
+        this.setNotificationIds(notificationIds);
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public List<Long> getNotificationIds() {
+        return notificationIds;
+    }
+    public void setNotificationIds(List<Long> notificationIds) {
+        this.notificationIds = notificationIds;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateUserWithNotificationDTO [name=" + name + ", surname=" + surname + ", mail=" + mail + ", password="
+                + password + ", notificationIds=" + notificationIds + "]";
+    }
+}
+
---
+++ src/main/java/com/example/financial_app/DTOs/UserDTO.java
@@ -0,0 +1,41 @@
+package com.example.financial_app.DTOs;
+
+public class UserDTO {
+    private String name;
+    private String surname;
+    private String mail;
+    private String password;
+
+    public UserDTO(){}
+    
+    public UserDTO(String name, String surname, String mail, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.mail = mail;
+        this.password = password;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
