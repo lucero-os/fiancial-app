18ce5c3c391b6e4428b5ca6612abb006ef902511
Osvaldo lucero <osvaldolcuero98@gmail.com>
2024-02-05 17:37:08 -0300

    UPDATE models/Subscription add constructor, getters and setters

--- src/main/java/com/example/financial_app/models/Subscription.java
+++ src/main/java/com/example/financial_app/models/Subscription.java
@@ -1,6 +1,7 @@
 package com.example.financial_app.models;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 import jakarta.persistence.*;
 
@@ -11,8 +12,30 @@ public class Subscription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Integer duration;
     private BigDecimal price;
+
+    @ManyToMany(mappedBy = "subscriptions", fetch = FetchType.LAZY)
+    private Set<User> users;
+
+    public Subscription(Long id, Integer duration, BigDecimal price) {
+        this.setDuration(duration);
+        this.setPrice(price);
+    }
+    public Long getId() {
+        return id;
+    }
+    public Integer getDuration() {
+        return duration;
+    }
+    private void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    private void setPrice(BigDecimal price) {
+        this.price = price;
+    }
     
 }
