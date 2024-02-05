f196146ea18a67ae58bac4f154ff62fbef6d6cf7
Osvaldo lucero <osvaldolcuero98@gmail.com>
2024-02-05 17:39:30 -0300

    UPDATE UserController add getUser method, change response types

--- src/main/java/com/example/financial_app/controllers/UserController.java
+++ src/main/java/com/example/financial_app/controllers/UserController.java
@@ -5,14 +5,20 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.financial_app.DTOs.UserDTO;
+import com.example.financial_app.DTOs.User.CreateUserDTO;
+import com.example.financial_app.DTOs.User.UserDTO;
+import com.example.financial_app.exceptions.CustomException;
 import com.example.financial_app.services.UserService;
+import com.example.financial_app.utils.ResponseUtil;
 
 @RestController
 @RequestMapping("/users")
@@ -20,19 +26,44 @@ public class UserController {
     @Autowired 
     UserService userService;
 
-    @GetMapping("/all")
-    public Map<String, Object> getUsers(){
+    @PostMapping("/create")
+    public Map<String, Object> createUser(@RequestBody CreateUserDTO user) throws CustomException{
         Map<String, Object> response = new HashMap<>();
-        List<UserDTO> users = this.userService.getUsers();
-        response.put("users", users);
+
+        this.userService.createUser(user);
+
+        response.put("message", "User created succesfully");
+        
         return response;
     }
 
-    @PostMapping("/create")
-    public Map<String, Object> createUser(@RequestBody UserDTO user){
+    @GetMapping("/all")
+    public ResponseEntity<ResponseUtil.ApiResponse<Object>> getUsers(){
+        Map<String, Object> data = new HashMap<>();
+        
+        List<UserDTO> users = this.userService.getUsers();
+        data.put("users", users);
+        
+        return ResponseUtil.success(data);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ResponseUtil.ApiResponse<Object>> getUser(@PathVariable Long userId) throws CustomException{
+        Map<String, Object> data = new HashMap<>();
+
+        UserDTO user = this.userService.getUserById(userId);
+        data.put("user", user);
+
+        return ResponseUtil.success(data);
+    }
+
+    @DeleteMapping("/{userId}")
+    public Map<String, Object> deleteUser(@PathVariable Long userId) throws CustomException{
         Map<String, Object> response = new HashMap<>();
-        this.userService.createUser(user);
-        response.put("message", "User created succesfully");
+        
+        this.userService.deleteUser(userId);
+        response.put("message", "User deleted succesfully");
+        
         return response;
     }
 }
